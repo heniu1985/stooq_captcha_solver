e0b64ecac172c864ced14a6e741de58b90a398ec
Damian Hennek <heniu_1@o2.pl>
2022-09-27 22:52:51 +0200

    Solving

--- captcha_solver.py
+++ captcha_solver.py
@@ -1,8 +1,15 @@
+import time
+from unittest import result
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from twocaptcha import TwoCaptcha
+
+from api_key import API_KEY
 
 url = "https://stooq.pl/db/h/"
 
@@ -10,19 +17,38 @@ driver = webdriver.Chrome(executable_path="chromedriver.exe")
 driver.get(url)
 driver.maximize_window()
 
+driver.implicitly_wait(5)
+
 cookie_button = driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/div[2]/div[2]/button[1]/p")
 cookie_button.click()
 
-driver.implicitly_wait(5)
+driver.implicitly_wait(15)
 
 download_link = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table[5]/tbody/tr/td/table/tbody/tr[2]/td[1]/table/tbody/tr[5]/td[3]/a")
 download_link.click()
 
-driver.implicitly_wait(10)
+driver.implicitly_wait(15)
 
 captcha_img = driver.find_element(By.ID, "cpt_cd")
 
 with open("captcha.png", "wb") as captcha:
     captcha.write(captcha_img.screenshot_as_png)
+
+solver = TwoCaptcha(API_KEY)
+
+captcha_solve = solver.normal("captcha.png")
+
+id = solver.send(file = "captcha.png")
+time.sleep(30)
+
+code = solver.get_result(id)
+
+captcha_text = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table[5]/tbody/tr/td/div[1]/div[1]/table/tbody/tr/td/table/tbody/tr[5]/td/input")
+captcha_text.send_keys(code)
+
+captcha_button = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table/tbody/tr/td/table[5]/tbody/tr/td/div[1]/div[1]/table/tbody/tr/td/table/tbody/tr[6]/td/input")
+captcha_button.click()
+
+time.sleep(5)
 
 driver.close()
